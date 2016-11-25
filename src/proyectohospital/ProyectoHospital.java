2d26457b8859a8eef86d494bc1cb12fa27d0de22
Felipe77 <carlosfandradev7b@hotmail.com>
2016-11-25 11:44:50 -0500

    Update ProyectoHospital.java

--- src/proyectohospital/ProyectoHospital.java
+++ src/proyectohospital/ProyectoHospital.java
@@ -15,7 +15,7 @@ public class ProyectoHospital {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola amigos");
         // TODO code application logic here
     }
     

7767e12e3e05ab76df31bdf4413e16018838c9b2
Jose7198 <jose719810@hotmail.com>
2016-11-25 11:38:46 -0500

    Prueba

--- src/proyectohospital/ProyectoHospital.java
+++ src/proyectohospital/ProyectoHospital.java
@@ -15,6 +15,7 @@ public class ProyectoHospital {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
         // TODO code application logic here
     }
     

6ccec8843f9f6b76616d60798b6cb5235dac4b4e
Jose7198 <jose719810@hotmail.com>
2016-11-26 00:18:41 -0500

    Update ProyectoHospital.java

--- src/proyectohospital/ProyectoHospital.java
+++ src/proyectohospital/ProyectoHospital.java
@@ -14,10 +14,47 @@ public class ProyectoHospital {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hola amigos");
-        System.out.println("irfji");
-        // TODO code application logic here
+     public static void main(String[] args) {
+        Hospital h=new Hospital();
+        Scanner sc=new Scanner(System.in);
+        int inicio;
+        do{
+            System.out.println("Sistema Hospitalario del "+h.getNombre());
+            System.out.println("Calificación: "+h.getCalificacion());
+            System.out.println("¿Qué desea hacer?");
+            System.out.println("1. Iniciar Sesión");
+            System.out.println("2. Escoger otro hospital");
+            System.out.println("3. Salir");
+            System.out.println("Encuentrenos en: "+h.getDireccion().toString());
+            inicio=sc.nextInt();
+            switch(inicio){
+                default:
+                    System.out.println("Por favor, seleccione una opción válida");
+                    break;
+                case 1:
+                    System.out.println("¿Qué tipo de usuario es?");
+                    break;
+                case 2:
+                    System.out.print("Ingrese el nombre del hospital: ");
+                    String nombreh=sc.nextLine();
+                    System.out.print("Ingrese la calificación del "+nombreh+": ");
+                    String calificacion=sc.nextLine();
+                    System.out.print("Ingrese la capacidad del "+nombreh+": ");
+                    int capacidadh=sc.nextInt();
+                    System.out.print("Ingrese la calle principal del "+nombreh+": ");
+                    String principal=sc.nextLine();
+                    System.out.println("Ingrese la calle secundaria del "+nombreh+": ");
+                    String secundaria=sc.nextLine();
+                    System.out.println("Ingrese el numero de calle del "+nombreh+": ");
+                    String numero=sc.nextLine();
+                    h.setHospital(nombreh, calificacion, capacidadh, principal, secundaria, numero);
+                    System.out.println(h.getNombre()+" ingresado con exito");
+                    break;
+                case 3:
+                    System.out.println("Gracias por usar nuestro servicio");
+                    break;
+            }
+        }while(inicio !=3);
     }
     
 }
